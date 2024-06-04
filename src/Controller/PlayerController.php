fe5c851e9e6f8aa40deb117089bd873cb3078915
F00LIM4N <leoaznargarrido@gmail.com>
2024-06-04 16:24:41 +0200

    push pour dams

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Manager\DbService;
 
 class PlayerController extends AbstractController
 {
+    public function __construct(
+        private DbService $dbService,
+    ) { }
+
     #[Route('/player', name: 'app_player')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $this->dbService->addPlayer($request->get('gameName'));
+
         return $this->render('player/index.html.twig', [
-            'controller_name' => 'PlayerController',
+            'prenom' => $request->get('gameName'),
+            'players' => $this->dbService->getPlayers(),
         ]);
     }
 }
