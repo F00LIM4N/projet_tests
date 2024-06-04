77ec0a234048adec066b1560e2efde25b6e31108
F00LIM4N <leoaznargarrido@gmail.com>
2024-06-04 15:57:15 +0200

    :art: structure de code

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -6,26 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Manager\DbService;
-use Psr\Log\LoggerInterface;
 
 class TestController extends AbstractController
 {
-
-    private $dbService;
-
-    public function __construct(DbService $dbService, LoggerInterface $logger)
-    {
-        $this->dbService = $dbService;
-        $this->logger = $logger;
-    }
-
+    public function __construct(
+        private DbService $dbService,
+    ) { }
 
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
         $players = $this->dbService->getPlayers();
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
             'players' => $players,
         ]);
     }
